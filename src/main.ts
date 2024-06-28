ae255e70e88f32d9d07ea2fe3f2f38ead28b098c
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-06-28 16:52:50 +0200

    Updates the imports

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 import axios from "axios";
 
 type Character = {
